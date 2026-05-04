335ce192141a115551de74147909e8173b92e116
gustavoch <gustavo.francelino@gmail.com>
2026-05-04 10:23:07 -0300

    Initial commit

---
+++ env.d.ts
@@ -0,0 +1,13 @@
+import type { D1Database, KVNamespace } from "@cloudflare/workers-types";
+
+declare global {
+    interface CloudflareEnv {
+    DATABASE: D1Database;
+    KV: KVNamespace<string>;
+        BETTER_AUTH_SECRET: string;
+        BETTER_AUTH_URL: string;
+        BETTER_AUTH_TRUSTED_ORIGINS: string;
+    }
+}
+
+export {};
